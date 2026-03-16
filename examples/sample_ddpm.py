23d8abc59d5dae51d2dac360a6457aacdf10eea9
Kalash Jain <kalashjain513@gmail.com>
2026-03-16 19:09:28 +0530

    fix: remove ema from sampling for small training

--- examples/sample_ddpm.py
+++ examples/sample_ddpm.py
@@ -1,11 +1,10 @@
 from difflow.models.ddpm import DDPM
 from torchvision.utils import save_image
 import torch
-from difflow.nn.ema import EMA
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-checkpoint = torch.load("ddpm_checkpoint_100.pth", map_location=device)
+checkpoint = torch.load("ddpm_checkpoint_500.pth", map_location=device)
 
 model = DDPM(
     beta_start=1e-4,
@@ -14,15 +13,9 @@ model = DDPM(
 model.load_state_dict(checkpoint['model'])
 model.eval()
 
-ema = EMA(model)
-ema.ema_weights = checkpoint['ema']
-ema.apply()
-
 n_samples = 4
 
 with torch.no_grad():
     samples = model.sample((n_samples, 3, 32, 32))
-    
-ema.restore()
     
 save_image(samples.cpu(), "samples.png", nrow=4, normalize=True)
